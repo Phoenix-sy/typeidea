c44278049ea46efab95ac1bde165ef5dfc4fa9b7
song <song@songdeMacBook-Pro.local>
2020-03-10 22:21:15 +0800

    配置mysql

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -4,8 +4,12 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea_db',
+        'USER': 'root',
+        'PASSWORD': '1234567890',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
